396732ccac479dd79315fbdffbed9ebea20cc55f
may65 <may65@inbox.ru>
2020-04-13 21:18:45 +0300

    budget init

---
+++ test/js.js
@@ -0,0 +1,15 @@
+// let x = 5; console.log( x++ );
+// console.log([ ] + false - null + true);
+let y = 1; let x = y = 2; 
+console.log(x);
+// alert(x); 
+console.log(typeof([ ] + 1 + 2) );
+console.log("1"[0]);
+console.log(2 && 1 && null && 0 && undefined);
+let a=1, b=2;
+console.log(!!( a && b ));
+console.log(( a && b ));
+console.log(( null || 2 && 3 || 4 ));
+a = [1, 2, 3]; b = [1, 2, 3]; console.log(a == b);
+console.log(typeof( +"Infinity" ));
+console.log( 0 || "" || 2 || undefined || true || falsе );
